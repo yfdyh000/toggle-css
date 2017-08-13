c1e3a8cd096923ad122f3a686b4e481cbcaf8ca0
Jonathan Kim <quailiff@gmail.com>
2017-08-13 15:04:48 -0400

    Toggle CSS; set button icon to match CSS state

--- src/background.js
+++ src/background.js
@@ -1,16 +1,70 @@
-function toggleCss() {
-    browser.tabs.query({
-            active: true,
-            currentWindow: true
-        })
-        .then((tabs) => {
-            tabs.forEach((tab) => {
-                // Need to tell the tab whether to enable or disable CSS
-                browser.tabs.sendMessage(tab.id, {
-                    disableCss: true
-                });
-            });
-        });
+var disabledTabIds = [];
+
+function updateIcon(activeTabId) {
+    var iconPath;
+    if (!activeTabId || disabledTabIds.includes(activeTabId)) {
+        iconPath = {
+            16: "assets/css-disabled.svg",
+            32: "assets/css-disabled.svg"
+        };
+    } else {
+        iconPath = {
+            16: "assets/css-enabled.svg",
+            32: "assets/css-enabled.svg"
+        };
+    }
+
+    browser.browserAction.setIcon({
+        path: iconPath,
+        tabId: activeTabId
+    });
+}
+
+function tabCssIsDisabled(tabId) {
+    return disabledTabIds.includes(tabId);
+}
+
+function setTabCssDisabled(tabId, disabled) {
+    if (disabled) {
+        if (!tabCssIsDisabled(tabId)) {
+            disabledTabIds.push(tabId);
+        }
+    } else {
+        // Remove tabId from disabledTabIds
+        disabledTabIds.splice(disabledTabIds.indexOf(tabId), 1);
+    }
+    updateIcon(tabId);
+}
+
+function handleTabUpdate(tabId) {
+    // TODO: Reset tab state if URL changed
+    // setTabCssDisabled(tabId, false);
+    updateIcon(tabId);
+}
+
+function toggleCss(activeTab) {
+    if (!activeTab.id) {
+        return;
+    }
+
+    var disableCss = !tabCssIsDisabled(activeTab.id);
+    if (disableCss) {
+        setTabCssDisabled(activeTab.id, true);
+    } else {
+        setTabCssDisabled(activeTab.id, false);
+    }
+
+    // Tell the content script to enable/disable CSS
+    browser.tabs.sendMessage(activeTab.id, {
+        disableCss
+    });
 }
 
 browser.browserAction.onClicked.addListener(toggleCss);
+
+browser.tabs.onRemoved.addListener(resetTabState);
+
+browser.tabs.onActivated.addListener(updateIcon);
+browser.tabs.onReplaced.addListener(updateIcon);
+
+browser.tabs.onUpdated.addListener(handleTabUpdate);
